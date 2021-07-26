b981052441ae23379f0f6aca1fa1e3d45488ae28
Novychan <tornado.strike2011@gmail.com>
2021-07-26 18:01:54 +0200

    Added MainActivity Added MainActivity Layout
    Started working on MainActivity Layout -> toolbar, chipgroup + display icon

---
+++ app/src/main/java/sprechstunde/community/themenschaedel/MainActivity.java
@@ -0,0 +1,40 @@
+package sprechstunde.community.themenschaedel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import java.util.Objects;
+
+import sprechstunde.community.themenschaedel.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
+
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        Toolbar toolbar = mBinding.activityMainToolbar;
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_search_settings, menu);
+        return true;
+    }
+}
